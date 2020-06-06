c13d0389e532c706510f42735e4adec7592b82cb
Andranik Markaryan <andranik.markaryan@betconstruct.com>
2020-06-06 20:37:32 +0400

    Feature - Classrooms id added

---
+++ app/Http/Resources/ClassroomResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Requests\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ClassroomResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'participants' => $this->participants,
+            'images' => []
+        ];
+    }
+}
